42fe6f2e2841800a36f4b4c36be76814653de062
aizikov <mikslakesha@gmail.com>
2024-04-28 20:29:22 +0300

    first

---
+++ app.py
@@ -0,0 +1,94 @@
+from flask import Flask, render_template, redirect, url_for, flash, request, session
+from models import User, Admin, db
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
+app.config['SECRET_KEY'] = '98919753708b5e27a2611fab552e2cce'
+
+# Инициализация SQLAlchemy
+db.init_app(app)
+
+# Создание таблиц при запуске приложения
+with app.app_context():
+    db.create_all()
+
+    # Проверяем, есть ли уже учетная запись администратора
+    admin = Admin.query.first()
+    if not admin:
+        # Создаем учетную запись администратора
+        admin = Admin(username='admin', password='h')
+        db.session.add(admin)
+        db.session.commit()
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if request.method == 'POST':
+        username = request.form['username']
+        email = request.form['email']
+        password = request.form['password']
+        user = User(username=username, email=email)
+        user.set_password(password)
+        db.session.add(user)
+        db.session.commit()
+        flash('Вы успешно зарегистрировались!', 'success')
+        return redirect(url_for('login'))
+    return render_template('register.html')
+
+@app.route('/admin', methods=['GET', 'POST'])
+def admin():
+    if request.method == 'POST':
+        password = request.form['password']
+        admin = Admin.query.first()
+        if admin and admin.check_password(password):
+            session['admin'] = True
+            return redirect(url_for('users'))
+        else:
+            flash('Неверный пароль', 'danger')
+    return render_template('admin.html')
+
+@app.route('/users', methods=['GET', 'POST'])
+def users():
+    if 'admin' in session and session['admin']:
+        if request.method == 'POST':
+            # Обработка изменения роли пользователя
+            user_id = request.form['user_id']
+            new_role = request.form['new_role']
+            user = User.query.get(user_id)
+            user.role = new_role
+            db.session.commit()
+            flash('Роль пользователя обновлена', 'success')
+            return redirect(url_for('users'))
+
+        users = User.query.all()
+        return render_template('users.html', users=users)
+    else:
+        return redirect(url_for('admin'))
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        user = User.query.filter_by(username=username).first()
+        if user and user.check_password(password):
+            session['user_id'] = user.id
+            flash('Вы успешно вошли в систему!', 'success')
+            return redirect(url_for('dashboard'))
+        else:
+            flash('Неверное имя пользователя или пароль', 'danger')
+    return render_template('login.html')
+
+@app.route('/dashboard')
+def dashboard():
+    if 'user_id' in session:
+        user = User.query.get(session['user_id'])
+        return render_template('dashboard.html', user=user)
+    else:
+        return redirect(url_for('login'))
+
+if __name__ == '__main__':
+    app.run(debug=True,host='0.0.0.0', port=5000)
---
+++ models.py
@@ -0,0 +1,32 @@
+from flask_sqlalchemy import SQLAlchemy
+from werkzeug.security import generate_password_hash, check_password_hash
+
+db = SQLAlchemy()
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    password = db.Column(db.String(100), nullable=False)
+    role = db.Column(db.String(20), default='student')
+
+    def __repr__(self):
+        return '<User %r>' % self.username
+
+    def set_password(self, password):
+        self.password = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password, password)
+
+class Admin(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    password_hash = db.Column(db.String(100), nullable=False)
+
+    def __init__(self, username, password):
+        self.username = username
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
